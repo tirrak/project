7c7f4d617998e0a253e91befbcf6d96612f2125a
Igor <cholomaloka@gmail.com>
2020-05-29 18:26:12 +0300

    Третье задание

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 "use strict";
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//Первое задание
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
-//Второе задание
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -12,4 +9,13 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-console.log(personalMovieDB.count);
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
